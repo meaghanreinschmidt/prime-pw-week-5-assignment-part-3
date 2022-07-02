b977515279162bcba32a64ecd1a908b613a38773
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-07-02 15:28:47 -0500

    created a function and object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+/**
+ * @param {string} title
+ * @param {string} artist
+ * @param {number} yearPublished
+ * @return {object} Returns new object  
+ */ 
+function addToCollection(title, artist, yearPublished) {
+    const newRecord = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    };
+    collection.push(newRecord);
+    return;
+}
