c4345b3f7ba0486cfbb389eecd9e8b901580e709
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-07-02 16:25:49 -0500

    created showCollection function and looped through collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,14 +14,24 @@ function addToCollection(title, artist, yearPublished) {
         yearPublished: yearPublished,
     };
     collection.push(newRecord);
-    return;
+    return newRecord;
 };
 
-addToCollection('American Teen', 'Khalid', 2017);
-addToCollection('Atlas:Enneagram', 'Sleeping At Last', 2019);
-addToCollection('The Click', 'AJR', 2017);
-addToCollection('The Lumineers', 'The Lumineers', 2012);
-addToCollection('WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?', 'Billie Eilish', 2019);
-addToCollection('folklore', 'Taylor Swift', 2020);
+console.log(addToCollection('American Teen', 'Khalid', 2017));
+console.log(addToCollection('Atlas:Enneagram', 'Sleeping At Last', 2019));
+console.log(addToCollection('The Click', 'AJR', 2017));
+console.log(addToCollection('The Lumineers', 'The Lumineers', 2012));
+console.log(addToCollection('WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?', 'Billie Eilish', 2019));
+console.log(addToCollection('folklore', 'Taylor Swift', 2020));
 console.log(collection);
 
+/**
+ * @param {array} recordsInput Array of record collection
+ */
+function showCollection(recordsInput) {
+    console.table(recordsInput);
+    for (let record of recordsInput) {
+        console.log(record.title + ' by ' + record.artist + ',', 'published in', record.yearPublished);
+    };
+}
+showCollection(collection);
