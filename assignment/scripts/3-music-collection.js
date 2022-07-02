43081608249de7d067c99f78dbf8c90260f79a73
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-07-02 15:53:25 -0500

    added 6 albums to record collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -15,4 +15,13 @@ function addToCollection(title, artist, yearPublished) {
     };
     collection.push(newRecord);
     return;
-}
+};
+
+addToCollection('American Teen', 'Khalid', 2017);
+addToCollection('Atlas:Enneagram', 'Sleeping At Last', 2019);
+addToCollection('The Click', 'AJR', 2017);
+addToCollection('The Lumineers', 'The Lumineers', 2012);
+addToCollection('WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?', 'Billie Eilish', 2019);
+addToCollection('folklore', 'Taylor Swift', 2020);
+console.log(collection);
+
